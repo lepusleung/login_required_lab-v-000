1a62522b054137ba7c500408119e1b621c506868
Man Leung <lepusleung@gmail.com>
2018-07-11 20:00:15 +0000

    automatically backed up by learn

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,28 @@
+class SessionsController < ApplicationController
+  def new
+
+  end
+
+  def create
+    if params[:name] == nil || params[:name] == ""
+      redirect_to login_path
+    else
+      session[:name] = params[:name]
+      redirect_to session_path(session[:name])
+    end
+  end
+
+  def show
+    @user = session[:name]
+  end
+
+  def destroy
+    if session[:name] != nil
+      session.delete :name
+    end
+    redirect_to login_path
+  end
+
+
+
+end
